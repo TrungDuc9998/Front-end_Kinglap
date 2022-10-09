0a50c835f5525ff14c46fc91fd95d0ab4f0bcce5
duc.nt <81727369+TrungDuc9998@users.noreply.github.com>
2022-10-09 18:19:19 +0700

    init commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,23 +1,28 @@
-import Home from '../pages/Home'
-import Category from '../pages/Category'
+import Home from "../pages/Home";
+import Category from "../pages/Category";
+import Product from "../pages/Product/Product";
+import { Children, Component } from "react";
+import CreateProduct from "../pages/Product/CreateProduct";
 // import Statistical from '../pages/Statistical'
 const publicRoutes = [
-    {
-        path:'/',
-        component: Home
-    },
-    {
-        path: '/category',
-        component: Category
-    },
-    // {
-    //     path: '/statistical',
-    //     component: Statistical
-    // }
-]
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/category",
+    component: Category,
+  },
+  {
+    path: "/product",
+    component: Product,
+  },
+  {
+    path: "/product/create",
+    component: CreateProduct,
+  },
+];
 
-const privateRoutes = [
+const privateRoutes = [];
 
-]
-
-export {publicRoutes,privateRoutes}
+export { publicRoutes, privateRoutes };
