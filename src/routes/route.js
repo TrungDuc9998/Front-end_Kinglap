4daed834aa95f027c286d6eac32b77c8a96c1e62
bangnv_techasians <bangnv@techasians.com>
2022-10-13 19:51:03 +0700

    up code quan ly nguoi dung

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,14 @@
-import Category from "../pages/Category";
 import Home from "../pages/Home";
-import Order from "../pages/Order/Order";
-import CreateProduct from "../pages/Product/CreateProduct";
+import Category from "../pages/Category";
 import Product from "../pages/Product/Product";
-import User from "../pages/User/User";
+// import { Children, Component } from "react";
+import CreateProduct from "../pages/Product/CreateProduct";
+import Order from "../pages/Order/Order";
+import Login from "../components/users/Login";
+import CreateOrder from "../pages/Order/CreateOrder";
+import ConfirmOrder from "../pages/Order/ConfirmOrder";
+import OrderSuccess from "../pages/Order/OrderSuccess";
+import OrderCancel from "../pages/Order/CancelOrder";
 // import Statistical from '../pages/Statistical'
 const publicRoutes = [
   {
@@ -27,12 +32,28 @@ const publicRoutes = [
     component: Order,
   },
   {
-    path: "/admin/user",
-    component: User,
+    path: "/login",
+    component: Login,
+    Layout: null,
   },
+  {
+    path: "/admin/order/confirm",
+    component: ConfirmOrder,
+  },
+  {
+    path: "/admin/order/success",
+    component: OrderSuccess,
+  },
+  {
+    path: "/admin/order/cancel",
+    component: OrderCancel,
+  },
+  {
+    path: "/admin/order/create",
+    component: CreateOrder,
+  }
 ];
 
 const privateRoutes = [];
 
 export { publicRoutes, privateRoutes };
-
