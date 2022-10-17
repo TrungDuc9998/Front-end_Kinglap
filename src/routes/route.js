a638894be1e1a6e405bda1b78ad19a330f370060
tranhuyhieu <81691983+tranhuyhieu@users.noreply.github.com>
2022-10-17 14:07:41 +0700

    hieu_qldv

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,16 @@
 import Home from "../pages/Home";
 import Category from "../pages/Category";
 import Product from "../pages/Product/Product";
-import { Children, Component } from "react";
+// import { Children, Component } from "react";
 import CreateProduct from "../pages/Product/CreateProduct";
 import Order from "../pages/Order/Order";
-import Discount from "../pages/Tag/Discount";
-// import Statistical from '../pages/Statistical'
+import Login from "../components/users/Login";
+import CreateOrder from "../pages/Order/CreateOrder";
+import ConfirmOrder from "../pages/Order/ConfirmOrder";
+import OrderSuccess from "../pages/Order/OrderSuccess";
+import OrderCancel from "../pages/Order/CancelOrder";
+import User from "../pages/User/User";
+import Discount from "../pages/Discount";
 const publicRoutes = [
   {
     path: "/",
@@ -31,6 +36,31 @@ const publicRoutes = [
     path: "/admin/discount",
     component: Discount,
   },
+  {
+    path: "/login",
+    component: Login,
+    Layout: null,
+  },
+  {
+    path: "/admin/order/confirm",
+    component: ConfirmOrder,
+  },
+  {
+    path: "/admin/order/success",
+    component: OrderSuccess,
+  },
+  {
+    path: "/admin/order/cancel",
+    component: OrderCancel,
+  },
+  {
+    path: "/admin/order/create",
+    component: CreateOrder,
+  },
+  {
+    path: "/admin/user",
+    component: User,
+  }
 ];
 
 const privateRoutes = [];
