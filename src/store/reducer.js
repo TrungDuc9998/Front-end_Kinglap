5363350f87a568d5045e3d0ac63e070e0cb4a2ff
tranhuyhieu <81691983+tranhuyhieu@users.noreply.github.com>
2022-11-05 23:07:49 +0700

    hieu_fix_cart

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,35 +1,40 @@
 import { ADD_TO_CART, CHECK_OUT_CART, CHANGE_CART_QTY, REMOVE_CART} from './constants'
 const initState = {
     cartCheckout: [],
-    cart: []
+    cart: JSON.parse(localStorage.getItem('carts'))?JSON.parse(localStorage.getItem('carts')):[]
 }
 
 function reducer(state, action) {
     switch (action.type) {
         case CHECK_OUT_CART: {
             return {
-                
-                // cart:state.cart.filter(c=>c.id!==action.payload.id),
+                ...state,
                 cartCheckout: action.payload,
             }
         }
         case ADD_TO_CART: {
-            return {
-                ...state,
+            state= {
+                ...state.cartCheckout,
                 cart:  [...state.cart, { ...action.payload, quantity:1, total:0}],
             }
+            localStorage.setItem('carts',JSON.stringify(state.cart));
+            return state;
         }
         case CHANGE_CART_QTY:{
-            return {
+            state= {
                 ...state,
                 cart:state.cart.filter(c=>c.id===action.payload.id?c.quantity=action.payload.quantity:c.quantity),
             }
+            localStorage.setItem('carts',JSON.stringify(state.cart));
+            return state;
         }
         case REMOVE_CART:{
-            return {
+            state=  {
                 ...state,
                 cart:state.cart.filter(c=>c.id!==action.payload.id),
             }
+            localStorage.setItem('carts',JSON.stringify(state.cart));
+            return state;
         }
         default:
             throw new Error('Invalid action!')
