fd5747a99db50bd88461e7807cc61f5524efd4cb
tranhuyhieu <81691983+tranhuyhieu@users.noreply.github.com>
2022-11-09 22:04:09 +0700

    hieu_fixcart

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,10 @@
 import { ADD_TO_CART, CHECK_OUT_CART, CHANGE_CART_QTY, REMOVE_CART} from './constants'
 const initState = {
     cartCheckout: [],
-    cart: JSON.parse(localStorage.getItem('carts'))?JSON.parse(localStorage.getItem('carts')):[]
+    cart: JSON.parse(localStorage.getItem('carts'))?JSON.parse(localStorage.getItem('carts')):[],
 }
-
 function reducer(state, action) {
+    
     switch (action.type) {
         case CHECK_OUT_CART: {
             return {
@@ -13,12 +13,26 @@ function reducer(state, action) {
             }
         }
         case ADD_TO_CART: {
-            state= {
-                ...state.cartCheckout,
-                cart:  [...state.cart, { ...action.payload, quantity:1, total:0}],
+            let data_add_cart = action.payload
+            const add_cart = state.cart
+            let indexCart = -1;
+            indexCart =(JSON.parse(localStorage.getItem('carts'))?JSON.parse(localStorage.getItem('carts')):add_cart).findIndex(value => {
+                return value.id === data_add_cart.id
+            })
+            data_add_cart.quantity=1
+            if (indexCart==-1){
+                add_cart.push(data_add_cart)
+                state = {
+                    ...state.cartCheckout,
+                    cart:add_cart
+                }
+            }else{
+                add_cart[indexCart].quantity = parseInt(add_cart[indexCart].quantity) + 1
+                console.log("Update")
             }
             localStorage.setItem('carts',JSON.stringify(state.cart));
-            return state;
+            return state
+            
         }
         case CHANGE_CART_QTY:{
             state= {
